e0beedfb7c61a3e233c1dc6cff3000f8f8de79d0
Zubair Cheema <cheema4310@gmail.com>
2024-03-14 18:26:29 +0500

    complete landing page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,39 +6,63 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <section id="footer">
-      <div className="flex items-center justify-around">
-        <div className="">
-          <h4>FIND US</h4>
-          <a href="mailto:support@investmentcompass.in">
+      <div className="flex items-center justify-around p-12 bg-darker text-lighter">
+        <div className="flex-1 flex flex-col items-center gap-2">
+          <h4 className="my-heading text-lightest">Connect with us</h4>
+          <a className="my-para" href="mailto:support@investmentcompass.in">
             support@investmentcompass.in
           </a>
-          <div className="flex">
-            <img className="w-8 h-8" src={youtubeSVG} alt="social icon" />
-            <img className="w-8 h-8" src={instaSVG} alt="social icon" />
-            <img className="w-8 h-8" src={linkedSVG} alt="social icon" />
+          <div className="my-flex gap-4 pt-4">
+            <a href="https://www.youtube.com/@TheInvestmentCompass">
+              <img
+                className="my-social-icon"
+                src={youtubeSVG}
+                alt="social icon"
+              />
+            </a>
+            <a href="https://www.instagram.com/theinvestmentcompass/">
+              <img
+                className="my-social-icon"
+                src={instaSVG}
+                alt="social icon"
+              />
+            </a>
+            <a href="https://www.linkedin.com/company/theinvestmentcompass/about/">
+              <img
+                className="my-social-icon"
+                src={linkedSVG}
+                alt="social icon"
+              />
+            </a>
           </div>
         </div>
-        <div>
-          <ul>
-            <li>
+        <div className="flex-1">
+          <ul className="flex flex-col items-center">
+            <li className="my-link">
               <Link to="/courses">Have a Question?</Link>
             </li>
-            <li>
+            <li className="my-link">
               <Link to="/courses">Our Courses</Link>
             </li>
-            <li>
+            <li className="my-link">
               <Link to="/events">All Events</Link>
             </li>
-            <li>
+            <li className="my-link">
               <Link to="/events">About Us</Link>
             </li>
           </ul>
         </div>
-        <div>
-          <h4>Don't Miss Out!</h4>
-          <form>
-            <input type="email" placeholder="Email Address" />
-            <button type="submit">Submit</button>
+        <div className="flex-1">
+          <h4 className="my-heading-normal py-2">Don't Miss Out!</h4>
+          <form className="flex flex-col items-center gap-4">
+            <input
+              className="my-input"
+              type="email"
+              placeholder="Email Address"
+            />
+            <button className="my-btn-light w-full" type="submit">
+              Submit
+            </button>
           </form>
         </div>
       </div>
